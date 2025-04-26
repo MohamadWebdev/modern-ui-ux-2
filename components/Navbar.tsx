d36de757c32491f94c143e2c5ca8d6fe41e9bc7a
MohamadWebdev <mohamad.webdev99@gmail.com>
2025-04-26 04:12:27 -0700

    navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import Button from './Button'
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between mt-5'>
-      <h1 className='font-IMFellGreatPrimerSC text-[23.35px] text-white'>Light</h1>
+    <div className="w-full flex justify-between mt-5">
+      <h1 className="font-IMFellGreatPrimerSC text-[23.35px] text-white">
+        Light
+      </h1>
 
-      <div className='flex'>
-        <Button style='gradient-1' title='Sign In' />
+      <div className="flex justify-center items-center">
+        <ul className="font-medium flex mr-10 gap-10 selected transition-all">
+          <li id="Home" className="cursor-pointer">
+            Home
+          </li>
+          <li id="Company" className="cursor-pointer">
+            Company
+          </li>
+          <li id="Features" className="cursor-pointer">
+            Features
+          </li>
+        </ul>
 
-        <ul></ul>
+        <Button style="gradient-1" title="Sign In" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
